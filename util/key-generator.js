f2157a9b58c5ca6a8f2650584b612b80be2f49e3
ofer221 <ofer2212@gmail.com>
2021-11-25 17:14:52 +0200

    prettify

--- util/key-generator.js
+++ util/key-generator.js
@@ -1,12 +1,13 @@
-const keyGenerator = (length, chars)=> {
-    var mask = '';
-    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
-    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (chars.indexOf('#') > -1) mask += '0123456789';
-    if (chars.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
-    var result = '';
-    for (var i = length; i > 0; --i) result += mask[Math.round(Math.random() * (mask.length - 1))];
-    return result;
-}
+const keyGenerator = (length, chars) => {
+  var mask = "";
+  if (chars.indexOf("a") > -1) mask += "abcdefghijklmnopqrstuvwxyz";
+  if (chars.indexOf("A") > -1) mask += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (chars.indexOf("#") > -1) mask += "0123456789";
+  if (chars.indexOf("!") > -1) mask += "~`!@#$%^&*()_+-={}[]:\";'<>?,./|\\";
+  var result = "";
+  for (var i = length; i > 0; --i)
+    result += mask[Math.round(Math.random() * (mask.length - 1))];
+  return result;
+};
 
-module.exports = keyGenerator
+module.exports = keyGenerator;
