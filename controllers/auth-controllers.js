a8ad971ce4026b34ac70e7ad52bebc4adec0798b
ofer221 <ofer2212@gmail.com>
2021-11-26 12:28:23 +0200

    Creating http error handler

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,13 +1,32 @@
+const User = require("../models/user-model");
 const keyGenerator = require("../util/key-generator");
+const HttpError = require("../util/http-error");
 
 const getAuthKey = async (req, res, next) => {
   try {
+    if (!req.body.id) {
+      throw new HttpError("Missing id", 422);
+    }
     const apiKey = keyGenerator(8, "A#");
+    const remainingMilliseconds = 10 * 60 * 1000;
+    let user = await User.findOne({ pid: req.body.id });
+    if (!user) {
+      user = new User({ pid: req.body.id });
+      await user.save();
+    }
+    user.apiKey = {
+      key: apiKey,
+      expiryDate: new Date(new Date().getTime() + remainingMilliseconds),
+    };
+    await user.save();
+
     res.status(200).json({
       message: "new key created successfully",
       data: { userId: req.params.userId, apiKey },
     });
-  } catch (e) {}
+  } catch (e) {
+    next(e);
+  }
 };
 
 module.exports = { getAuthKey };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authRoutes = require("./routes/auth-routes");
 const flightRoutes = require("./routes/flight-routes");
 const mongoose = require("mongoose");
+const HttpError = require('./util/http-error')
 
 const port = process.env.PORT || 3000;
 
@@ -12,6 +13,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/flights", flightRoutes);
+
+app.use((req, res, next) => {
+    throw new HttpError('Could not find this route.', 404);
+});
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
+});
 
 mongoose.connect('mongodb://localhost/flights').then(()=>{
     app.listen(port, () => console.log(`server listening on port ${port}`));
