aeeb51bf2521578d9bfa331baee60fa4d8388076
ofer221 <ofer2212@gmail.com>
2021-12-01 12:57:41 +0200

    fix 2 for validator middleware

--- middleware/objectIdValidation-middleware.js
+++ middleware/objectIdValidation-middleware.js
@@ -2,7 +2,7 @@ const HttpError = require("../util/http-error");
 const ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports = (req, res, next) => {
-    if(ObjectId.isValid(req.params.id)){
+    if("^[0-9a-fA-F]{24}$".test(req.params.id)){
         return next();
     }
     throw new HttpError("Invalid id", 422);
